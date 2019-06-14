f734d8bc48e4bf5bc4c496317fde8bba7a357a65
Andy Bettisworth <andy@accreu.com>
2019-06-14 10:43:19 -0500

    Setup the database

--- server/db/migrations/20180810082348_users.js
+++ server/db/migrations/20180810082348_users.js
@@ -1,15 +1,14 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('users', users => {
-    users.increments();
+'use strict'
 
-    users
-      .string('username', 255)
-      .notNullable()
-      .unique();
-    users.string('password', 255).notNullable();
-  });
-};
+exports.up = function(knex) {
+  return knex.schema.createTable('users', table => {
+    table.increments()
+    table.text('username').notNullable().unique()
+    table.text('password_hash').notNullable()
+    table.timestamps(true, true)
+  })
+}
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('users');
-};
+  return knex.schema.dropTableIfExists('users')
+}
