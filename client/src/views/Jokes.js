57301505aae36235947f0957b1d176ec7df0d62a
Andy Bettisworth <andy@accreu.com>
2019-06-14 12:32:57 -0500

    Try to render jokes

--- client/src/views/Jokes.js
+++ client/src/views/Jokes.js
@@ -2,25 +2,48 @@
  * Dependencies
  */
 
-import React from 'react'
+import axios from 'axios'
+import React, { Component } from 'react'
 import { JokesStyle } from './styles/index'
 
 /**
  * Define component
  */
 
-function Jokes() {
-  return (
-    <JokesStyle>
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-10 col-md-8 col-lg-5" style={{ marginTop: '80px' }}>
-            <h3>Jokes</h3>
+class Jokes extends Component {
+  state = {
+    jokes: []
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8080/jokes')
+      .then(res => {
+        this.setState(() => ({ jokes: res.data }))
+      })
+      .catch(({ response }) => {
+        console.error(response)
+      })
+  }
+
+  render() {
+    return (
+      <JokesStyle>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-10 col-md-8 col-lg-5" style={{ marginTop: '80px' }}>
+              <h3>Jokes</h3>
+
+              <ul>
+                {this.state.jokes.map(joke => (
+                  <li key={joke.id}>{joke.joke}</li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </JokesStyle>
-  )
+      </JokesStyle>
+    )
+  }
 }
 
 /**
